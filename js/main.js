a68a6eb9b73bb3e0071d6f9f834ef835288895e2
Your Namepaco <you@example.com>
2017-03-15 18:58:34 +0100

    First commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function test(){
+	//alert ("hola")
+        
+    for (i = 0; i < 5; i++) {
+    var mitag = document.getElementById('texto1'); mitag +="<p class='text_red'>soy el nuevo texto</p>";
+    }
+}
